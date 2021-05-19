122d03454208e7fb5c4704deba6db67873740dab
Bertrand Gadaix <bg@innosense.eu>
2021-05-19 16:36:08 +0200

    Updated ImageListener cpp file for consistency with .h file

--- Src/Libraries/MatisseImage/image_listener.cpp
+++ Src/Libraries/MatisseImage/image_listener.cpp
@@ -1,7 +1,4 @@
 ﻿#include "image_listener.h"
 using namespace MatisseCommon;
 
-//ImageListener::ImageListener(QObject *parent) :
-//    QObject(parent)
-//{
-//}
+// Constructor implemented by derived classes
